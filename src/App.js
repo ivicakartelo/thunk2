2b367901c8b700e827218274b62469f0336fe607
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-08-18 19:10:59 +0200

    container css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import {PostsList} from './features/posts/PostsList'
 
 function App() {
     return (
-        <>
+        <div className='container'>
             <AddPostForm />
             <PostsList />
-        </>
+        </div>
     )
 }
 export default App
