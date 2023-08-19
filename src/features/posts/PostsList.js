4f3e7eba2565a6577683127f6e9cba1b41634581
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-08-19 18:06:34 +0200

    React app Material UI Card

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { selectAllPosts, fetchPosts, handleDelete } from './postsSlice'
 import { UpdatePostForm } from './UpdatePostForm'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const PostExcerpt = ({ post }) => {
     const [showEditForm, setShowEditForm] = useState(false);
@@ -12,13 +17,101 @@ const PostExcerpt = ({ post }) => {
         setUpdateId(id);
         setShowEditForm(true);
       }
+/*
+      const card = (
+        <React.Fragment>
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div">
+              be*nev*o*lent
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2">
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </React.Fragment>
+      );
 
-    return (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
+      const card = (
+        <React.Fragment>
+          <CardContent>
+            <Typography sx={{ fontSize: 14, fontFamily: 'Dancing Script', cursive }} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div" style={{ fontFamily: 'Dancing Script', cursive }}>
+              be*nev*o*lent
+            </Typography>
+            <Typography sx={{ mb: 1.5, fontFamily: 'Dancing Script', cursive }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" style={{ fontFamily: 'Dancing Script', cursive }}>
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </React.Fragment>
+      );
+    
 
-            {showEditForm && updateId === post.id ? (
+      const card = (
+        <React.Fragment>
+          <CardContent>
+            <Typography sx={{ fontSize: 14, fontFamily: 'Dancing Script', 'cursive' }} color="text.secondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="div" style={{ fontFamily: 'Dancing Script', 'cursive' }}>
+              be*nev*o*lent
+            </Typography>
+            <Typography sx={{ mb: 1.5, fontFamily: 'Dancing Script', 'cursive' }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" style={{ fontFamily: 'Dancing Script', 'cursive' }}>
+              well meaning and kindly.
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </React.Fragment>
+      );
+  */    
+
+      const card = (
+        <React.Fragment>
+          <CardContent>
+            <Typography sx={{ fontSize: 14, fontFamily: 'Dancing Script' }} color="text.secondary" gutterBottom>
+              Published
+            </Typography>
+            <Typography variant="h5" component="div" style={{ fontFamily: 'Dancing Script' }}>
+            {post.title}
+            </Typography>
+            <Typography sx={{ mb: 1.5, fontFamily: 'Dancing Script' }} color="text.secondary">
+              Author
+            </Typography>
+            <Typography variant="body2" style={{ fontFamily: 'Dancing Script' }}>
+            {post.content}
+              <br />
+              {'"a benevolent smile"'}
+            </Typography>
+          </CardContent>
+          <CardActions>
+          {showEditForm && updateId === post.id ? (
                 <UpdatePostForm
                     post={post}
                     setShowEditForm={setShowEditForm}
@@ -30,6 +123,13 @@ const PostExcerpt = ({ post }) => {
             )}
 
             <button onClick={() => dispatch(handleDelete(post.id))}>Delete</button>
+          </CardActions>
+        </React.Fragment>
+      );
+
+    return (
+        <article key={post.id}>
+            <Card variant="outlined">{card}</Card>
         </article>
     )
 }
