43187d66c5fc0f1b0bf54a04c1d5c3bf80d042ed
ivicakartelo <ivicakartelo@users.noreply.github.com>
2023-08-25 00:13:35 +0200

    CSS Flex Overlap with images in React app

--- src/template/CardCSSFlexOverlap.js
+++ src/template/CardCSSFlexOverlap.js
@@ -4,13 +4,23 @@ export default function CardCSSFlexOverlap() {
   return (
 <div class="flex-container">
   <div class="left">
-    <div class="item overlap">Item 1</div>
-    <div class="item overlap shifted">Item 2</div>
+    <div class="item overlap">
+      <img src={process.env.PUBLIC_URL + 'assets/images/1.jpg'} alt={'image 1'} />
+    </div>
+    <div class="item overlap shifted">
+      <img src={process.env.PUBLIC_URL + 'assets/images/2.jpg'} alt={'image 2'} />
   </div>
-  <div class="middle"></div>
+  </div>
+  <div class="middle">
+    <img src={process.env.PUBLIC_URL + 'assets/images/7.jpg'} alt={'image 7'} />
+  </div>
   <div class="right">
-    <div class="item overlap">Item 3</div>
-    <div class="item overlap shifted">Item 4</div>
+    <div class="item overlap">
+      <img src={process.env.PUBLIC_URL + 'assets/images/3.jpg'} alt={'image 3'} />
+    </div>
+    <div class="item overlap shifted">
+      <img src={process.env.PUBLIC_URL + 'assets/images/4.jpg'} alt={'image 4'} />
+    </div>
   </div>
 </div>
   )
